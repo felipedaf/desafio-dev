4274b6ce74ab01e3fa0a62191a5ec26d023bc5f2
FelipeAmorimFerreira <felipe.amorim.ferreira@ccc.ufcg.edu.br>
2021-11-13 16:15:00 -0300

    File upload endpoint

--- api/src/modules/cnab/cnab.controller.ts
+++ api/src/modules/cnab/cnab.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CnabService } from './cnab.service';
 
 @Controller('cnab')
-export class CnabController {}
+export class CnabController {
+  constructor(private readonly cnabService: CnabService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  upload(@UploadedFile() file: Express.Multer.File) {
+    return this.cnabService.upload(file);
+  }
+}
--- api/src/modules/cnab/cnab.service.ts
+++ api/src/modules/cnab/cnab.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CnabService {}
+export class CnabService {
+  upload(file) {
+    const content = file.buffer.toString('utf-8');
+    console.log(file);
+    console.log(content);
+    return { message: 'Upload successfully done!' };
+  }
+}
